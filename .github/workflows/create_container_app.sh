b07d4eed053c38367f91eb179555188b370daf60
BickyShaw <bicky.shaw@capgemini.com>
2024-05-25 18:18:57 +0530

    first commit

--- .github/workflows/create_container_app.sh
+++ .github/workflows/create_container_app.sh
@@ -2,7 +2,7 @@ templateFile="./bicep/main.bicep"
 uuid="$(cat /proc/sys/kernel/random/uuid)"
 parameterFile="./bicep/main.json"
 
-
+az provider register --namespace Microsoft.App
 az deployment group create --resource-group demo-rg --template-file $templateFile --parameters $parameterFile
 
 
